99bb313a96163bd215396039687e54803a69ca81
LesetjaModiba <104294464+LesetjaModiba@users.noreply.github.com>
2022-10-30 18:12:27 +0200

    Add files via upload

--- src/components/addFile.js
+++ src/components/addFile.js
@@ -75,9 +75,9 @@ const AddPatient = () =>{
                     <input type='text' className="textInput" onChange={(e)=>setPhysicalAddress(e.target.value)} placeholder="Physical Address"/><br></br>
                     <p>Condition: </p>
                     <select style={{width:"80%",height:35,background: "rgba(255, 255, 255, 0.2)",border:"none",borderRadius:5}} placeholder="Condition" onChange={(e)=>setCondition(e.target.value)}>
-                    <option value="High">Mild</option>
-                    <option value="Medium">Moderate</option>
-                    <option value="Low">Severe</option>
+                    <option value="Mild">Mild</option>
+                    <option value="Moderate">Moderate</option>
+                    <option value="Severe">Severe</option>
                 </select>
                     {/* <input type='text' className="textInput" onChange={(e)=>setCondition(e.target.value)} placeholder="Condition"/><br></br> */}
                     <input type='text' className="textInput" onChange={(e)=>setNotes(e.target.value)} placeholder="Notes"/><br></br>
